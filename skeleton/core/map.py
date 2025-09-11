0ce14c7ad3a897773dca6da3608cc0031c3273e8
wellnaxx <highonosu@gmail.com>
2025-09-11 18:45:09 +0300

    update map

--- skeleton/core/map.py
+++ skeleton/core/map.py
@@ -1,29 +1,15 @@
 class Map:
     _locations = ["SYD", "MEL", "ADL", "ASP", "BRI", "DAR", "PER"]
 
-    _distances = _distances = {
-        ("SYD", "MEL"): 877,
-        ("SYD", "ADL"): 1376,
-        ("SYD", "ASP"): 2762,
-        ("SYD", "BRI"): 909,
-        ("SYD", "DAR"): 3935,
-        ("SYD", "PER"): 4016,
-        ("MEL", "ADL"): 725,
-        ("MEL", "ASP"): 2255,
-        ("MEL", "BRI"): 1765,
-        ("MEL", "DAR"): 3752,
-        ("MEL", "PER"): 3509,
-        ("ADL", "ASP"): 1530,
-        ("ADL", "BRI"): 1927,
-        ("ADL", "DAR"): 3027,
-        ("ADL", "PER"): 2785,
-        ("ASP", "BRI"): 2993,
-        ("ASP", "DAR"): 1497,
-        ("ASP", "PER"): 2481,
-        ("BRI", "DAR"): 3426,
-        ("BRI", "PER"): 4311,
-        ("DAR", "PER"): 4025,
-    }
+    _distances = {
+    "SYD": {"MEL": 877, "ADL": 1376, "ASP": 2762, "BRI": 909, "DAR": 3935, "PER": 4016},
+    "MEL": {"SYD": 877, "ADL": 725, "ASP": 2255, "BRI": 1765, "DAR": 3752, "PER": 3509},
+    "ADL": {"SYD": 1376, "MEL": 725, "ASP": 1530, "BRI": 1927, "DAR": 3027, "PER": 2785},
+    "ASP": {"SYD": 2762, "MEL": 2255, "ADL": 1530, "BRI": 2993, "DAR": 1497, "PER": 2481},
+    "BRI": {"SYD": 909, "MEL": 1765, "ADL": 1927, "ASP": 2993, "DAR": 3426, "PER": 4311},
+    "DAR": {"SYD": 3935, "MEL": 3752, "ADL": 3027, "ASP": 1497, "BRI": 3426, "PER": 4025},
+    "PER": {"SYD": 4016, "MEL": 3509, "ADL": 2785, "ASP": 2481, "BRI": 4311, "DAR": 4025},
+}
 
     @classmethod
     def get_locations(cls):
@@ -31,18 +17,16 @@ class Map:
 
     @classmethod
     def get_distance(cls, city_a, city_b):
-        if city_a.strip().upper() == city_b.strip().upper():
-            return 0
-        
-        key = (city_a.strip().upper(), city_b.strip().upper())
-        reverse_key = (city_b.strip().upper(), city_a.strip().upper())
+        city_a = city_a.strip().upper()
+        city_b = city_b.strip().upper()
 
-        if key in cls._distances:
-            return cls._distances[key]
-        elif reverse_key in cls._distances:
-            return cls._distances[reverse_key]
-        else:
-            raise ValueError(f"No known distance between {city_a} and {city_b}")
+        if city_a not in cls._locations:
+            raise ValueError(f"Invalid location: {city_a}")
+        if city_b not in cls._locations:
+            raise ValueError(f"Invalid location: {city_b}")
+        if city_a == city_b:
+            raise ValueError("Invalid input. First and second location cannot be the same")
+        return cls._distances[city_a][city_b]
         
     @classmethod
     def is_valid_location(cls, location):
