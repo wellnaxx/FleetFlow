7746cba201cdae27cece1f721cb587ef105275cb
wellnaxx <highonosu@gmail.com>
2025-09-11 18:46:03 +0300

    add functionality to delivery package

--- skeleton/models/delivery_package.py
+++ skeleton/models/delivery_package.py
@@ -1,4 +1,6 @@
 from core.map import Map
+from models.item_status import ItemStatus
+from datetime import timedelta
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -12,12 +14,13 @@ class DeliveryPackage:
         self.start_location = start_location
         self.end_location = end_location
         self.weight = weight
-        self.customer = customer._user_id
+        self.customer = customer
 
         self._package_id = DeliveryPackage._next_id
         DeliveryPackage._next_id += 1
 
         self.route = None
+        self.status = ItemStatus.TODO
     
     @property
     def start_location(self):
@@ -67,3 +70,24 @@ class DeliveryPackage:
         if value is not None and not isinstance(value, DeliveryRoute):
             raise ValueError("Route must be a DeliveryRoute object or None.")
         self._route = value
+
+    @property
+    def contact_info(self):
+        return f"{self.customer.name} ({self.customer.email}, {self.customer.phone_number})"
+    
+    @property
+    def calculate_km(self, start_location, end_location):
+        return Map.get_distance(start_location, end_location)
+    
+    def info(self):
+        return(
+            f"Package id: {self.package_id}\n"
+            f"Start location: {self.start_location}\n"
+            f"End location: {self.end_location}\n"
+            f"Weight: {self.weight}\n"
+            f"Customer contacts: {self.contact_info}\n"
+            f"Status: {self.status}\n"
+            f"Departure time: {self.route.departure_time if self.route else 'Not assigned to a route'}\n"
+            f"Arrival time: {self.route.departure_time + timedelta(hours=self.calculate_km / self.route._average_speed_kmh) if self.route else 'Not assigned to a route'}"
+        )
+    
