1e9100adcb7344f9b56e14d2e2f5547d637478d1
wellnaxx <highonosu@gmail.com>
2025-09-11 18:47:14 +0300

    add functionality to customer

--- skeleton/models/users/customer.py
+++ skeleton/models/users/customer.py
@@ -2,8 +2,8 @@ from models.users.user import User
 from models.delivery_package import DeliveryPackage
 
 class Customer(User):
-    def __init__(self, name, phone_number, email, user_id):
-        super().__init__(name, phone_number, email, user_id)
+    def __init__(self, name, phone_number, email):
+        super().__init__(name, phone_number, email)
         self._delivery_packages: list[DeliveryPackage] = []
 
     @property
@@ -11,22 +11,15 @@ class Customer(User):
         return tuple(self._delivery_packages)
     
     def _existing_package(self, package: DeliveryPackage) -> bool:
-        if not isinstance(package, DeliveryPackage):
-            raise TypeError("Only DeliveryPackage instances can be checked.")
         return any(p.package_id == package.package_id for p in self._delivery_packages)
 
 
     def add_package(self, package: DeliveryPackage):
-        if not isinstance(package, DeliveryPackage):
-            raise TypeError("Only DeliveryPackage instances can be added.")
-        
         if self._existing_package(package):
             raise ValueError(f"Package with id {package.package_id} is already assigned to this customer.")
         self._delivery_packages.append(package)
 
     def remove_package(self, package: DeliveryPackage):
-        if not isinstance(package, DeliveryPackage):
-            raise TypeError("Only DeliveryPackage instances can be removed.")
         if not self._existing_package(package):
             raise ValueError(f"Package with id {package.package_id} does not exist.")
         self._delivery_packages.remove(package)
