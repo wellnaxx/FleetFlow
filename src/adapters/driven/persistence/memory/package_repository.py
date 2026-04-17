0faa3d792ec577627aebf3865917f67f4ef8f17d
wellnaxx <highonosu@gmail.com>
2026-04-17 16:52:33 +0300

    feat: create an in-memory package repository adapter

---
+++ src/adapters/driven/persistence/memory/package_repository.py
@@ -0,0 +1,27 @@
+from src.domain.entities.delivery_package import DeliveryPackage
+
+
+class InMemoryPackageRepository:
+    def __init__(self) -> None:
+        self._packages: dict[int, DeliveryPackage] = {}
+
+    def next_id(self) -> int:
+        if not self._packages:
+            return 1
+        return max(self._packages.keys()) + 1
+
+    def add(self, package: DeliveryPackage) -> None:
+        self._packages[package.package_id] = package
+
+    def remove(self, package_id: int) -> None:
+        if package_id in self._packages:
+            del self._packages[package_id]
+
+    def get_by_id(self, package_id: int) -> DeliveryPackage | None:
+        return self._packages.get(package_id)
+    
+    def list_all(self) -> list[DeliveryPackage]:
+        return list(self._packages.values())
+    
+    def list_unassigned(self) -> list[DeliveryPackage]:
+        return [package for package in self._packages.values() if package.route is None]
