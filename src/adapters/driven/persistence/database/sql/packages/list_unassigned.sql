c84947d6171cffaa1f71eeeb1e1f7691bdc7f892
wellnaxx <highonosu@gmail.com>
2026-05-01 12:18:11 +0300

    feat(db): list unassigned packages sql query

---
+++ src/adapters/driven/persistence/database/sql/packages/list_unassigned.sql
@@ -0,0 +1,18 @@
+SELECT
+    p.package_id,
+    p.start_location,
+    p.end_location,
+    p.weight,
+    p.status,
+    p.current_location,
+    p.expected_arrival,
+    p.customer_id,
+    p.route_id,
+    c.name AS customer_name,
+    c.email AS customer_email,
+    c.phone AS customer_phone
+FROM public.packages p
+JOIN public.customers c
+    ON c.customer_id = p.customer_id
+WHERE p.status = 'To Do' AND p.route_id IS NULL
+ORDER BY p.package_id;
