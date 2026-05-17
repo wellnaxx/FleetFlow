6e0884341020668f4e4c1bde066798e93ed9001c
wellnaxx <highonosu@gmail.com>
2026-05-17 10:56:17 +0300

    feat(sql): add SQL script to increment token version by username

---
+++ src/adapters/driven/persistence/database/sql/users/increment_token_version_by_username.sql
@@ -0,0 +1,12 @@
+UPDATE public.users
+SET token_version = token_version + 1
+WHERE username = %s
+RETURNING
+    user_id,
+    username,
+    role,
+    name,
+    email,
+    phone,
+    password_hash,
+    token_version;
