6028a0a3e57c93c811d6bf2bbe6bbdcae970dc91
wellnaxx <highonosu@gmail.com>
2026-05-24 16:23:18 +0300

    feat(sql): add SQL query to list trucks assigned by routes

---
+++ src/adapters/driven/persistence/database/sql/trucks/list_assigned_by_routes.sql
@@ -0,0 +1,15 @@
+SELECT
+    t.vehicle_id,
+    t.name,
+    t.capacity,
+    t.max_range,
+    t.status,
+    t.current_location,
+    t.busy_from,
+    t.busy_until,
+    t.in_transit_to
+FROM public.routes r
+JOIN public.trucks t
+    ON t.vehicle_id = r.truck_vehicle_id
+WHERE r.route_id = ANY(%s)
+ORDER BY t.vehicle_id;
